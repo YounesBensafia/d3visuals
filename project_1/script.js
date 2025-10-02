1c32f53c6ef588a2d108458d3d2e1326e8fcc3c6
Younes Bensafia <younes.bensafia@outlook.com>
2025-10-03 00:51:57 +0100

    Update SVG width and refactor circle creation logic in script.js

--- project_1/script.js
+++ project_1/script.js
@@ -2,27 +2,25 @@ window.onload = function () {
   const svg = d3
     .select("#chart")
     .append("svg")
-    .attr("width", 300)
+    .attr("width", 1500)
     .attr("height", 200);
 
-  d3.csv("./data/named_colors_css.csv")
-    .then((data) => {
-      data.forEach((d, index) => {
-        console.log(`${index}: ${d.Name} - ${d.Hex}`);
-      });
-
-      if (data.length > 4) {
-        svg
-          .append("circle")
-          .attr("cx", 150)
-          .attr("cy", 100)
-          .attr("r", 50)
-          .style("fill", data[8].Hex);
-
-        console.log("Circle created with color:", data[8].Name, data[8].Hex);
-      }
-    })
-    .catch((error) => {
-      console.error("Error loading CSV:", error);
+  d3.csv("./data/named_colors_css.csv").then((data) => {
+    data.forEach((d, index) => {
+      // console.log(`${index}: ${d.Name} - ${d.Hex}`);
     });
+
+    const spacing = 70; 
+    const startX = 50;
+    
+    for (let i = 0; i < data.length; i++) {
+      console.log(`Creating circle ${i} with color: ${data[i].Name} - ${data[i].Hex}`);
+      svg
+      .append("circle")
+      .attr("cx", startX + (i * spacing))
+      .attr("cy", 100)
+      .attr("r", 30)
+      .style("fill", data[i].Hex);
+    }
+  });
 };
