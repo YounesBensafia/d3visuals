2b684a3c6bb49e2de667cb47b417cb4ed4332eb8
Younes Bensafia <younes.bensafia@outlook.com>
2025-10-03 01:05:05 +0100

    Refactor circle positioning logic and clean up unused code in script.js

--- project_1/script.js
+++ project_1/script.js
@@ -3,12 +3,11 @@ window.onload = function () {
     .select("#chart")
     .append("svg")
     .attr("width", 1500)
-    .attr("height", 200);
+    .attr("height", 200)
+    .style("border", "1px solid black")
+    .style("background-color", "red");
 
   d3.csv("./data/named_colors_css.csv").then((data) => {
-    data.forEach((d, index) => {
-      // console.log(`${index}: ${d.Name} - ${d.Hex}`);
-    });
 
     const spacing = 70;
     const startX = 50;
@@ -17,10 +16,13 @@ window.onload = function () {
       console.log(
         `Creating circle ${i} with color: ${data[i].Name} - ${data[i].Hex}`
       );
+      const row = Math.floor(i / 10);
+      const col = i % 10;
+
       svg
         .append("circle")
-        .attr("cx", startX + i * spacing)
-        .attr("cy", 100)
+        .attr("cx", startX + col * spacing)
+        .attr("cy", 100 + row * spacing)
         .attr("r", 30)
         .style("fill", data[i].Hex);
     }
